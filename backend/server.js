77fb656a0f872595051e5838ad66beba454c0252
Despo-coder <damion.wilson@gmail.com>
2023-11-25 19:02:45 -0500

    Updated dist folder and routing

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
@@ -37,3 +37,5 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
